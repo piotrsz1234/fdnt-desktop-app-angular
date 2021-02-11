a07d9c34d0f54aca611b07a4231569fd552101db
Piotr Szuflicki <piotrsz1098@gmail.com>
2021-02-11 15:41:22 +0100

    Added routing, layout and some base logic for application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { User } from './login/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fndt-angular-app';
+export class AppComponent implements OnInit {
+	title = 'FNDT Application for Web';
+
+	constructor(router: Router) {
+		let t = new User();
+		t.email = "piotr@dzielo.pl";
+		t.groups = ["Bialostocka"];
+		localStorage.setItem("user", JSON.stringify(t));
+		router.events.subscribe((ev) => {
+			return;
+			/*if (ev instanceof NavigationEnd) {
+				let json = localStorage.getItem("user");
+				if (json == null || json == "") router.navigate(["login"]);
+				else {
+					let user: User = new User();
+					user = JSON.parse(json);
+					if (user == null) router.navigate(["login"]);
+				}
+			}*/
+		});
+	}
+
+	onResize(event: any) {
+
+		let el = document.getElementById("navbar");
+		let content = document.getElementById("content");
+		if (el != null && content != null) {
+			content.style.height = (window.innerHeight - el.offsetHeight).toString() + "px";
+		}
+	}
+
+	public ngOnInit(): void {
+		this.onResize(null);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    RouterModule.forRoot([
+      {path: "login", component: LoginComponent},
+      {path: "calendar", component: CalendarComponent},
+      {path: '', redirectTo: '/calendar', pathMatch: 'full'}
+    ]),
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    LoginComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
