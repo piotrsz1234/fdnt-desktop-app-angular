33034a6313ed5a87e64477bbf4da7ceb8cabf117
KasiaWiktoria <kw_s@interia.pl>
2021-02-15 15:28:10 +0100

    Add news section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component'
+import { NewsComponent } from './news/news.component'
 import { RouterModule } from '@angular/router'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
@@ -23,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     RouterModule.forRoot([
       {path: "login", component: LoginComponent},
       {path: "calendar", component: CalendarComponent},
+      {path: "news", component: NewsComponent},
       {path: '', redirectTo: '/calendar', pathMatch: 'full'}
     ]),
     HttpClientModule
@@ -30,7 +32,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   declarations: [
     AppComponent,
     CalendarComponent,
-    LoginComponent
+    LoginComponent,
+    NewsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
