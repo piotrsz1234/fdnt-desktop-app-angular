e00939f9fcf337779b78a3a394f1b7ceda041288
Piotr Szuflicki <piotrsz1098@gmail.com>
2021-02-16 14:50:08 +0100

    Fix

--- src/app/tasklists/tasklist.ts
+++ src/app/tasklists/tasklist.ts
@@ -1,12 +1,12 @@
 import { emptyGuid } from '../config';
 
 export class TaskList {
-    iD : string = "";
+    id : string = "";
     name : string = "";
     owner : string = "";
 
     constructor() {
-        this.iD = emptyGuid;
+        this.id = emptyGuid;
         this.name = "Wybierz listę zadań";
     }
 }
