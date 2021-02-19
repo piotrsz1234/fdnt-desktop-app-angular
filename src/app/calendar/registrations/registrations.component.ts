c00c7fe1a19afd86704a9f421513a2ab997e49ce
Piotr Szuflicki <piotrsz1098@gmail.com>
2021-02-19 12:34:23 +0100

    Add component for preview and selection of registrations

---
+++ src/app/calendar/registrations/registrations.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { APICalendarEvent, Participation } from '../calendarEvent';
+import { CombineUrls, apiUrl, Where, GetUser } from 'src/app/config';
+import { HttpParams, HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { UserInfo } from 'src/app/login/user';
+
+declare let showToast: Function;
+
+@Component({
+  selector: 'app-registrations',
+  templateUrl: './registrations.component.html',
+  styleUrls: ['./registrations.component.css']
+})
+export class RegistrationsComponent implements OnInit {
+
+  @Input()
+  calendarEvents: APICalendarEvent[] = [];
+
+  participations: Participation[][] = [];
+
+  @Output()
+  areThereAny: EventEmitter<boolean> = new EventEmitter<boolean>();
+    
+  constructor(private http : HttpClient) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges | null): void {
+    let email = (GetUser() as UserInfo).email;
+    this.calendarEvents = Where(this.calendarEvents, (f: APICalendarEvent) => f.creatorEmail == email);
+    for (let k of this.calendarEvents)
+      this.fetchParticipationsForEvent(k);
+    this.areThereAny.emit(this.AreThereAnyToConfirm());
+  }
+
+  fetchParticipationsForEvent(e:APICalendarEvent ) : void {
+    let params = new HttpParams().set("eventID", e.id);
+    this.http.get<Participation[]>(CombineUrls(apiUrl, "Calendar/participations"), { params })
+      .subscribe((observer) => {
+        console.log(observer);
+        this.participations[this.calendarEvents.indexOf(e)] = [];
+        if (observer.length > 0)
+          for (let k of observer)
+            if(!k.hasOwnerConfirmed)
+              this.participations[this.calendarEvents.indexOf(e)].push(k);
+        else this.participations[this.calendarEvents.indexOf(e)] = new Array<Participation>();
+      }, (err : HttpErrorResponse) => {
+          showToast("Coś poszło nie tak :(");
+      });
+  }
+
+  getEventById(id: string): APICalendarEvent {
+    let result = Where(this.calendarEvents, (f: APICalendarEvent) => f.id == id);
+    return result[0];
+  }
+
+  emailToFancyName(email: string): string {
+    email = email.substr(0, email.indexOf('@'));
+    email = email.substr(0, 1).toUpperCase() + email.substr(1);
+    let index = email.indexOf('.') + 1;
+    email = email.substr(0, index) + email.substr(index, 1).toUpperCase() + email.substr(index + 1);
+    email = email.replace('.', ' ');
+    return email;
+  }
+
+  confirmParticipation(p: Participation) {
+    console.log(p.id);
+    this.http.post(CombineUrls(apiUrl, "Calendar/participation"), p)
+      .subscribe(() => {
+        showToast("Pomyślnie zaakceptowano");
+        this.ngOnChanges(null);
+      }, (err: HttpErrorResponse) => {
+          showToast("Coś poszło nie tak :(");
+    })
+  }
+
+  removeParticipation(p: Participation) {
+    let data = {
+      calendarEventID: p.calendarEventId,
+      owner: (GetUser() as UserInfo).email
+    }
+    const options = {
+			headers: new HttpHeaders({
+			  'Content-Type': 'application/json',
+			}),
+			body: data,
+		  };
+    this.http.delete(CombineUrls(apiUrl, "Calendar/participations"), options)
+      .subscribe(() => {
+        showToast("Udało się usunąć rejestrację");
+        this.fetchParticipationsForEvent(this.getEventById(p.id));
+      }, (err: HttpErrorResponse) => {
+          showToast("Cos poszło nie tak :(")
+    })
+  }
+
+  AreThereAnyToConfirm() {
+    let output = false;
+    for (let k of this.participations)
+      if (k.length > 0) output = true;
+    this.areThereAny.emit(output);
+    return output;
+  }
+
+}
